61bb0e38a2a490f65c23497f75098140234de152
Brendan Asselstine <mail.asselstine@gmail.com>
2020-01-02 15:23:32 -0800

    PoolTokenMock changes to inherit from RecipientWhitelistPoolToken

--- contracts/PoolTokenMock.sol
+++ contracts/PoolTokenMock.sol
@@ -1,5 +1,5 @@
 pragma solidity ^0.5.0;
 
-import "pooltogether-contracts/contracts/PoolToken.sol";
+import "pooltogether-contracts/contracts/RecipientWhitelistPoolToken.sol";
 
-contract PoolTokenMock is PoolToken {}
+contract PoolTokenMock is RecipientWhitelistPoolToken {}
